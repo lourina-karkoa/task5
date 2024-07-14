a2f5e4dc78873c83c28d06bc325ce8b38333b204
lourina-karkoa <nanasy893@gmail.com>
2024-07-15 01:21:56 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+
+function showsidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display='flex'
+}
+function hidesidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display='none'
+}
+// const nav = document.querySelector('.navbar2');
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY => 200) {
+//     nav.classList.add('nav2');
+//   }
+//   else if (window.scrollY < 200) {
+//     nav.classList.remove('nav2');
+//   }
+// });
+// const headEl = document.querySelector('.navbar2');
+// window.addEventListener('scroll',()=>{
+//     if (window.scrollY > 50){
+//         headEl.classList.add('nav2');
+//     }else if(window.scrollY <= 50){
+//         headEl.classList.remove('nav2');
+//     }
+
+// });
